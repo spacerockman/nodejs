8e9aa9ee24c974652a7e445ed4b05fa10fce305e
MAC <>
2020-10-23 18:36:21 +0900

    first commit

---
+++ easyserver.js
@@ -0,0 +1,18 @@
+//set http, hostname port
+
+//'require' is kind like 'import' in Java
+const http = require('http');
+
+const hostname = '127.0.0.1';
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/palin');
+    res.end('Hello, world');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
